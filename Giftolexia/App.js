25416936a68262d9700b9bb2df0e8037de9f936a
mehulpoddar <mehulpoddar@gmail.com>
2018-01-07 15:57:27 +0530

    App changes

--- Giftolexia/App.js
+++ Giftolexia/App.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 import firebase from 'firebase';
 import Root from './src/components/Root';
-import { Spinner } from './src/components/common';
-import LoginForm from './src/components/screens/LoginForm';
-import DashSel from './src/components/screens/DashSel';
 
 class App extends Component {
   state = { loggedIn: null }
@@ -16,22 +13,6 @@ class App extends Component {
       storageBucket: 'giftolexia-3c09f.appspot.com',
       messagingSenderId: '967747504832'
   });
-
-  firebase.auth().onAuthStateChanged((user) => {
-    if (user) {
-      this.setState({ loggedIn: true });
-    } else {
-      this.setState({ loggedIn: false });
-    }
-  });
-  }
-
-  renderContent() {
-   switch (this.state.loggedIn) {
-     case true: return <DashSel />;
-     case false: return <LoginForm />;
-     default: return <Spinner size="large" />;
-   }
   }
 
   render() {
