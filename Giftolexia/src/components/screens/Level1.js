7ba5c5e5a6bb3c420925e019049b4e20cfbb5b35
shreesh5 <shreeshnayak@gmail.com>
2018-01-07 20:36:57 +0530

    Level1 file

---
+++ Giftolexia/src/components/screens/Level1.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View } from 'react-native';
+import Header from '../common/Header';
+
+const Level1 = () => (
+  <View style={{ flex: 1 }}>
+    <Header headerText={'Level 1'} />
+  </View>
+);
+
+export default Level1;
