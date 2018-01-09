367d240850ba8efd61a98de5401aec11b7a798a6
mukundakdas <33874619+mukundakdas@users.noreply.github.com>
2018-01-10 00:21:07 +0530

    Added an exception handler incase of reset password failure

--- Giftolexia/src/components/screens/LoginForm.js
+++ Giftolexia/src/components/screens/LoginForm.js
@@ -59,14 +59,14 @@ export default class Login extends Component {
   }
 
   forgotPassword() {
-    if ((this.state.email) === '') {
-      Alert.alert('Empty Username! Please check again!');
-    }
-    else {
-    firebase.auth().sendPasswordResetEmail(this.state.email);
-    Alert.alert('Reset Email has been sent to your Email ID');
+    firebase.auth().sendPasswordResetEmail(this.state.email)
+    .then(Alert.alert('Reset Email has been sent to your Email ID!'))
+     .catch(this.forgotPasswordFail.bind(this));
   }
-}
+
+  forgotPasswordFail() {
+    Alert.alert('Please enter a valid Email ID!');
+  }
 
   render() {
     if (!this.state.loggedIn) {
