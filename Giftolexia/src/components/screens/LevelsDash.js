b8a6b09551c1e81aee2b9364693fd0f4d375ccfe
mehulpoddar <mehulpoddar@gmail.com>
2018-07-20 15:06:52 +0530

    small change

--- Giftolexia/src/components/screens/LevelsDash.js
+++ Giftolexia/src/components/screens/LevelsDash.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { ListView, View, Image, TouchableOpacity, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from '../common';
 import LevelDetail from '../LevelDetail';
-import firebase from 'firebase';
 
 const headerImage = require('../../images/gifto_logo.png');
 const l1 = require('../../images/level1.jpg');
