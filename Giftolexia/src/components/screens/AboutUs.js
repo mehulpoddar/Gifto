87ad3bee132eeeb0a38d25bb2c1561ef78f2579d
shreesh5 <shreeshnayak@gmail.com>
2018-11-07 12:22:57 +0530

    About Us

--- Giftolexia/src/components/screens/AboutUs.js
+++ Giftolexia/src/components/screens/AboutUs.js
@@ -1,26 +1,97 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity, Linking, Image } from 'react-native';
+const headerImage = require('../../images/gifto_logo.png');
 
 const AboutUs = () => {
   const {
-    buttonText,
+    mainContainer,
+    toolBar,
+    toolBarTitle,
+    content,
+    bottom,
+    //imageStyle,
+    contentStyle,
+    contentHeaderStyle,
   } = styles;
 
   return (
-    <View>
-          <Text style={buttonText}> About Us </Text>
+    <View style={mainContainer}>
+      <View style={toolBar}>
+          <Text style={toolBarTitle}>About Us</Text>
+      </View>
+      <View style={content}>
+          <Text style={contentHeaderStyle}>Welcome to the Phonetic way of learning English!</Text>
+          <Text />
+          <Text style={contentStyle}>
+            Phonetics is the study of speech-sounds. Phonics or letter-sound knowledge is the foundation needed to build reading and writing abilities.
+            Phonics instruction teaches you how to decode letters into their respective sounds, a skill that is essential for you to read unfamiliar words.
+            It improves your spelling.  It also helps you acquire the right intonation of a particular language and helps you pronounce words correctly.
+            We have curated content to help you learn to read in a systematic way. The various levels have content in the form of video, documents and practise sheets. We urge you to go through the entire content for each level.
+          </Text>
+          <Text/>
+          <Text style={contentStyle}>Happy Learning!</Text>
+      </View>
+      <View style={bottom}>
+        <Text style={contentStyle}>Write to us info@giftolexia.com for any clarification or comments with the Subject: English Learning App.</Text>
+        <Text />
+      </View>
     </View>
-
   );
 };
 
 const styles = {
-  buttonText: {
-    textAlign: 'center',
+  imageStyle: {
+    alignSelf: 'center',
+    bottom: 0,
+    height: 75,
+    width: 300,
+    position: 'absolute'
+  },
+  mainContainer: {
+    flex: 1
+  },
+  content: {
+    flex: 2,
+    alignItems: 'center'
+  },
+  toolBar: {
+    backgroundColor: '#20a3aa',
+    paddingTop: 10,
+    paddingBottom: 10,
+    flexDirection: 'row'
+  },
+  toolBarTitle: {
     color: '#fff',
+    textAlign: 'center',
+    flex: 1,
     fontWeight: '700',
     fontSize: 18
   },
+  bottom: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    marginTop: 100
+  },
+  contentHeaderStyle: {
+    fontSize: 16,
+    textAlign: 'center',
+    color: '#000'
+  },
+  contentStyle: {
+    fontSize: 16,
+    textAlign: 'justify',
+    color: '#000',
+    margin: 3
+  }
 };
+
+/*
+<View style={bottom}>
+    <Image
+      source={headerImage}
+      style={imageStyle}
+      resizeMode="stretch"
+    />
+</View>*/
 
 export default AboutUs;
